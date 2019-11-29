06fd594086280236fa3add24abb845e67ff84f38
Alvaro Zuniga <alvaro@chelista.com>
2019-11-29 00:59:26 -0500

    Adds Materialize CSS files and initial Navigation Bar

---
+++ client/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import '../node_modules/materialize-css/dist/css/materialize.css';
+    import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
+    import {Router} from 'svelte-routing';
+    import Navbar from './layout/Navbar.svelte';
+</script>
+
+<style>
+
+</style>
+
+<Router>
+    <Navbar />
+    <div class="container">
+
+    </div>
+</Router>
+
+
---
+++ client/src/layout/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {Link} from 'svelte-routing';
+</script>
+
+<nav class=b>
+    <div class="nav-wrapper">
+        <div class="container">
+            <Link to="/"><span class="brand-logo">OneRoster Enrollments</span></Link>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><Link to="/schools">Schools</Link></li>
+                <li><Link to="/classes">Classes</Link></li>
+                <li><Link to="/teachers">Teachers</Link></li>
+                <li><Link to="/students">Students</Link></li>
+            </ul>
+        </div>
+    </div>
+</nav>
